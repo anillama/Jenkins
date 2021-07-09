3e1f6b40f5b1d5682ec76e6876a45afd9f00abbe
anillama <33677877+anillama@users.noreply.github.com>
2021-07-09 16:03:04 -0600

    added files

---
+++ src/com/hello/jenkins/Jenkin.java
@@ -0,0 +1,11 @@
+package com.hello.jenkins;
+
+public class Jenkin {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Nepal");
+
+	}
+
+}
